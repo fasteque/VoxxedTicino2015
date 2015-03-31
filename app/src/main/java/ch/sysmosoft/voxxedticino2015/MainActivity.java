47c34f04b30c4349ef33ecb27827f18fb793e072
Daniele Altomare <daniele.altomare@sysmosoft.com>
2015-03-31 22:11:25 +0200

    UI improvements

--- app/src/main/java/ch/sysmosoft/voxxedticino2015/MainActivity.java
+++ app/src/main/java/ch/sysmosoft/voxxedticino2015/MainActivity.java
@@ -1,7 +1,10 @@
 package ch.sysmosoft.voxxedticino2015;
 
+import android.os.Build;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.transition.Explode;
+import android.transition.Fade;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,20 +15,27 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_voxxed);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            if(getSupportActionBar() != null) {
+                getSupportActionBar().setElevation(0);
+            }
+
+            getWindow().setNavigationBarColor(getResources().getColor(R.color.primary_dark));
+            getWindow().setEnterTransition(new Explode());
+            getWindow().setExitTransition(new Fade());
+            getWindow().setAllowEnterTransitionOverlap(true);
+        }
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
